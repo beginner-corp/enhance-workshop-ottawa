4e31feeda21e243972d9b3e2f1c55c4c8d959d40
Ryan Bethel <ryan.bethel@gmail.com>
2023-09-07 11:38:29 -0400

    added fonts to head

--- app/head.mjs
+++ app/head.mjs
@@ -1,5 +1,4 @@
 import { getStyles }  from '@enhance/arc-plugin-styles'
-
 const { linkTag } = getStyles
 
 export default function Head () {
@@ -9,10 +8,43 @@ export default function Head () {
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Enhance Starter Project</title>
-      ${linkTag()}
+      <meta name="og:type" content="website" />
       <link rel="icon" href="/_public/favicon.svg">
-      <meta name="description" content="The HTML first full stack web framework.">
+      <title>Axol Lotl: Web Developer</title>
+      <meta name="description" content="Portfolio for Axol Lotl, Senior Developer" />
+      ${linkTag()}
+
+      <style>
+        @font-face {
+          font-family: "HK Grotesk";
+          font-weight: 300;
+          src: url("/_public/fonts/HKGrotesk-Light.woff2") format("woff2")
+        }
+
+        @font-face {
+          font-family: "HK Grotesk";
+          font-weight: 400;
+          src: url("/_public/fonts/HKGrotesk-Regular.woff2") format("woff2")
+        }
+
+        @font-face {
+          font-family: "HK Grotesk";
+          font-weight: 600;
+          src: url("/_public/fonts/HKGrotesk-SemiBold.woff2") format("woff2")
+        }
+
+        body {
+          color: var(--dark);
+          text-rendering: optimizeLegibility;
+        }
+
+        a {
+          text-decoration: underline;
+          text-decoration-thickness: 0.0625em;
+          text-underline-offset: 0.0625em;
+        }
+      </style>
     </head>
+    <body class='font-sans'>
 `
 }
