202110c43e0958caab2d4b6a54123f4482d3973b
macdonst <simon.macdonald@gmail.com>
2023-09-08 09:22:16 -0400

    Add published to seed data

--- scripts/seed-data.mjs
+++ scripts/seed-data.mjs
@@ -4,35 +4,40 @@ async function main() {
     table: 'links',
     key: 'link1',
     text: 'Custom properties',
-    url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/--*'
+    url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/--*',
+    published: true
   })
 
   await db.set({
     table: 'links',
     key: 'link2',
     text: 'Calc',
-    url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/calc'
+    url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/calc',
+    published: true
   })
 
   await db.set({
     table: 'links',
     key: 'link3',
     text: 'Creating color themes with custom properties',
-    url: 'https://css-tricks.com/creating-color-themes-with-custom-properties-hsl-and-a-little-calc/'
+    url: 'https://css-tricks.com/creating-color-themes-with-custom-properties-hsl-and-a-little-calc/',
+    published: true
   })
 
   await db.set({
     table: 'links',
     key: 'link4',
     text: 'Theming with CSS Custom Properties (variables) and calc()',
-    url: 'https://itnext.io/theming-with-css-custom-properties-variables-and-calc-a89b37ad0013'
+    url: 'https://itnext.io/theming-with-css-custom-properties-variables-and-calc-a89b37ad0013',
+    published: true
   })
 
   await db.set({
     table: 'links',
     key: 'link5',
     text: 'Calculating Color: Dynamic Color Theming with Pure CSS',
-    url: 'https://una.im/css-color-theming/'
+    url: 'https://una.im/css-color-theming/',
+    published: true
   })
 }
 main()
